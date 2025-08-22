47db1242c06648add9e001db3a01feb25c651f81
Fasna4522 <fasna.musthafa4522@gmail.com>
2025-08-22 11:51:35 +0530

    Update TodoItem.jsx

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,7 @@
 function TodoItem({ todo, onDelete, onToggle }){
     return(
         <div className="todo-item">
-            <input type='checkbox' checked={todo.completed} onChange={()=>onToggle(toolbar._id,todo.completed)}/>
+            <input type='checkbox' checked={todo.completed} onChange={()=>onToggle(todo._id,todo.completed)}/>
             <span style={{textDecoration: todo.completed ? 'line-through' : 'none'}}>
             {todo.title}
             </span>
